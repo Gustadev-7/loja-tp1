c0eb2eecefd9251e9cff11695a57b0595ccf86cb
Gustadev-7 <gustavo.souzadomingos7@gmail.com>
2026-08-13 22:07:57 -0300

    aula 3

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,15 @@ import { Component, signal } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
-  protected readonly title = signal('Loja TP1');
+  titulo = input.required<string>(); // Chamada de API
+  textosobre = output<string>(); // Chamada de API
+
+  enviarSobre(): void {
+    this.textosobre.emit('Técnincas de Programação I.\nDesenvolvido por Gustavo')
+  }
+
+  exibirMensagem(msg: string): void {
+    alert(msg);
+  }
+
 }
